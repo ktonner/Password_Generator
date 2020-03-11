4191ffa8b88a96874917afea1c98b712dc7e9e29
Kathleen Tonner <kathleentonner@gmail.com>
2020-03-11 19:41:41 -0400

    Changed character variables from arrays to strings

--- script.js
+++ script.js
@@ -16,10 +16,10 @@ var specChar = confirm("Do you want special characters?")
 
 
 
-function generatePassword() {
-  var num = ['1234567890']
-  var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-  var spec = ['?!#%/()+-,<=>@[]~&*.:;^[\\]']
+function generatePassword(length = numChar) {
+  var num = '1234567890'
+  var letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  var spec = '?!#%/()+-,<=>@[]~&*.:;^[\\]'
 }
 
 
