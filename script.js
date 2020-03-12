1ba688ef549c643b40ff100627a6af125b1d5ae4
Kathleen Tonner <kathleentonner@gmail.com>
2020-03-12 18:27:44 -0400

    Added character length restriction.

--- script.js
+++ script.js
@@ -3,8 +3,12 @@ var generateBtn = document.querySelector("#generate");
 
 //Deciding length and complexity
 var numChar = prompt("How many characters would you like in your password?")
+while(numChar>120){
+  numChar= prompt("Passwords can only be 120 characters.")
+}
 var uppChar = confirm("Do you want upper case letters?")
 var specChar = confirm("Do you want special characters?")
+
 
 
 //Possible Characters
@@ -13,10 +17,17 @@ var char = '1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ?!#%/(
 //Declaring new password
 var newPass = ""
 
+//Declaring newPass
+document.getElementById("password").textContent = newPass
+
 //Generate password with loop
 generateBtn.addEventListener("click",function generatePassword() {
   console.log("Was clicked")
+  //Reset for each click
+  newPass = ""
   for(i=0; i<=numChar; i++){
+
+  //Conditionals set by confirms
     if(uppChar == false){
       char = char.slice(0,35) + char.slice(62)
     }
@@ -24,6 +35,7 @@ generateBtn.addEventListener("click",function generatePassword() {
       char = char.slice(0,62)
     }
     console.log(char)
+    //Making new password
     newPass=newPass + char.charAt(Math.floor(Math.random() * char.length))
   }
   console.log(newPass)
